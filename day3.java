3e285c58d7272819323a29cc7fb7a3bfafde08e5
Isdayunisari <isdai041@gmail.com>
2024-09-04 11:01:04 +0800

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,15 @@
+package days3;
+
+public class days3 {
+    
+    public static void main (String[] args) {
+        System.out.println("Nama = Isda yunisari");
+        System.out.println("Alamat = Papalang");
+        System.out.println("TTL = Mamuju 10 Juni 2006");
+        System.out.println("Jenis Kelamin = Perempuan");
+        System.out.println("Agama = Kristem");
+        System.out.println("Umur = 18 tahun");
+        
+      }
+    
+      }
