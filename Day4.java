1d88ef167cdbebee7d9be9606ca488dc59954e83
Isdayunisari <isdai041@gmail.com>
2024-09-05 17:30:50 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,12 @@
+package day4;
+
+public class day4 {
+    
+    public static void main(String[] args) {
+        System.out.print("Hai kak Nama Isda yunisari ");
+            System.out.printf("Angka favorite %s adalah", "4");
+        
+    }
+    
+    }
+    
