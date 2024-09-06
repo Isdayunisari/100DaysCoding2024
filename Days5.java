291f243a696c5d44d6c2686b4a77f6cfd141fce9
Isdayunisari <isdai041@gmail.com>
2024-09-06 14:52:29 +0800

    Create days5.java

---
+++ Days5.java
@@ -0,0 +1,18 @@
+package Days5;
+
+public class Days5 {
+    
+    public static void main(String[] args) {
+        String nama="Isda Yunisari";
+        
+        System.out.print("nama :" +nama+ " ");
+        //contoh print
+        
+        System.out.println("favorite EXO");
+        //Contoh println
+        
+        System.out.printf("Saya berusia %s tahun", "18");
+        //contoh printf
+    }  
+    
+    }
