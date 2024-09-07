d27410fc4a60a5758edfb7ed5255593fe2f4d5d4
Isdayunisari <isdai041@gmail.com>
2024-09-07 11:52:43 +0800

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,15 @@
+package Days6;
+
+public class Days6 {
+    
+    public static void main(String[] args) {
+        
+        int umur;
+        int tanggal;
+        int tahun;
+        String tempatLahir; 
+        
+        
+    }
+    
+    }
