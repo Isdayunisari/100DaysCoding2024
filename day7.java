3d0a5713397f72d06b8dccda4bed29b79e4df0b0
Isdayunisari <isdai041@gmail.com>
2024-09-08 19:03:11 +0800

    Create day7.java

---
+++ day7.java
@@ -0,0 +1,13 @@
+package Days7;
+
+public class Days7 {
+    
+    public static void main(String[] args) {
+        String nama = "Isda Yunisari";
+        String hobby= "Volly";
+        System.out.println("Nama saya adalah: " + nama);
+        System.out.println("Nama saya " + nama + "saya suka" + hobby);
+        
+    }
+    
+    }
