50e41ba648a7e5bf02af34e1be43bdd4d4ede280
Isdayunisari <isdai041@gmail.com>
2024-09-09 18:54:01 +0800

    Create day8.java

---
+++ day8.java
@@ -0,0 +1,25 @@
+package Days8;
+
+public class Days8 {
+    
+    public static void main(String[] args) {
+        //sebelum di update
+        String nama = "Isda";
+        System.out.println("nama: "+ nama);
+        //setelah diupdate
+        nama = "Baekhyun";
+        System.out.println("Nama setelah diupdate: "+ nama);
+        //Sebelum diupdate
+        int tanggal = 10;
+        System.out.println("tanggal: "+ tanggal);
+        //setelah diupdate
+        tanggal = 44;
+        System.out.println("tanggal setelah diupdate: "+ tanggal);
+        
+        
+    }
+        
+    }     
+        
+        
+    
