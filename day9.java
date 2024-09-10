e625fe29463efacaafa8295cc4d090f37f5ad2ab
Isdayunisari <isdai041@gmail.com>
2024-09-10 16:47:06 +0800

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,11 @@
+package Days9;
+
+public class Days9 {
+    
+    public static void main(String[] args) {
+        final double angka = 4.4;
+        System.out.println("angka "+ angka);
+        
+       }
+    
+       }
