9b4bdc342227fb5d5e9fc6a754d4f0b1bf89f540
Isdayunisari <isdai041@gmail.com>
2024-09-11 16:44:13 +0800

    Create day10.java

---
+++ day10.java
@@ -0,0 +1,14 @@
+package Days10;
+
+public class Days10 {
+    
+    public static void main(String[] args) {
+        byte minimal = -128;
+        byte maxsimal = 127;
+        System.out.println("minimal "+ minimal);
+        System.out.println("maxsimal "+ maxsimal);
+        
+    }
+        
+    
+    }
