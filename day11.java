85fff3e7e18e96ae0e225ee4ca8c27f17d2555af
Isdayunisari <isdai041@gmail.com>
2024-09-12 08:29:42 +0800

    Create day11.java

---
+++ day11.java
@@ -0,0 +1,13 @@
+package Days11 ;
+
+public class Days11 {
+    
+    public static void main(String[] args) {
+        short minimal = -32768;
+        short maxsimal = 32767;
+        System.out.println("Nilai minimal type data short adalah "+ minimal);
+        System.out.println("Nilai maxsimal type data short adalah "+ maxsimal);
+        
+    }
+    
+    }
