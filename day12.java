2918298f12d2440cbf82f2df904f63424cf315cf
Isdayunisari <isdai041@gmail.com>
2024-09-13 19:52:17 +0800

    Create day12.java

---
+++ day12.java
@@ -0,0 +1,14 @@
+package Days12;
+
+public class Days12 {
+    
+    public static void main(String[] args) {
+        int minimal = -214748448;
+        int maxsimal = 214748447;
+        System.out.println("Nilai minimal type data int adalah "+ minimal);
+        System.out.println("Nilai maxsimal type data int adalah "+ maxsimal);
+        
+    }
+        
+    
+ }
