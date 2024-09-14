3cd0334b3c1bd69aae9863b62be7734138b7f161
Isdayunisari <isdai041@gmail.com>
2024-09-14 21:43:57 +0800

    Create day13.java

---
+++ day13.java
@@ -0,0 +1,25 @@
+package Days13;
+
+public class Days13 {
+
+     public static void main(String[] args) {
+         final String nama = "isda yunisari";
+               double tinggi = 152.1;
+              
+               int umur = 18;
+               
+               System.out.println("nama "+ nama);
+               System.out.println("tinggi "+ tinggi);
+               System.out.println("umur "+ umur);
+               System.out.println("=======");
+               umur = 19;
+                tinggi = 160.1;
+               System.out.println("nama "+ nama);
+               System.out.println("tinggi "+ tinggi);
+               System.out.println("umur "+ umur);
+       
+    }
+    
+}
+    
+}
