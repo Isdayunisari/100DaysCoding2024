f230328be61112ac175f614020c624734676640a
Isdayunisari <isdai041@gmail.com>
2024-09-15 19:27:30 +0800

    Create day14.java

---
+++ day14.java
@@ -0,0 +1,14 @@
+package Days14;
+
+public class Days14 {
+    
+    
+     public static void main(String[] args) {
+         long umur = 18;
+         long umur1= 15;
+         System.out.println("Nilai tipe data long adalah "+ umur);
+          System.out.println("Nilai tipe data long adalah "+ umur1);
+          
+     }
+     
+}
