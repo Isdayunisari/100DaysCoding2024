1fa4f3aa311e70500fb06d5eccb2ce56746b76b4
Isdayunisari <isdai041@gmail.com>
2024-09-16 22:17:51 +0800

    Create day15.java

---
+++ day15.java
@@ -0,0 +1,13 @@
+package Days15;
+
+public class Days15 {
+    
+    public static void main(String[] args) {
+        double tinggibadan = 152.4;
+        double bb = 50.2;
+        System.out.println("tinggibadan type data double adalah "+ tinggibadan);
+        System.out.println("bb type data double adalah "+ bb);
+            
+    } 
+    
+}
