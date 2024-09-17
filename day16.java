fd3a381b4d74803d39c1f82fd69a2dfed247e75e
Isdayunisari <isdai041@gmail.com>
2024-09-17 18:38:19 +0800

    Create day16.java

---
+++ day16.java
@@ -0,0 +1,13 @@
+package Days16;
+
+public class Days16 {
+    
+    public static void main(String[] args) {
+        float Angka1 = 122.4f;
+        float Angka2 = 244.6f;
+        System.out.println("nilai type data float adalah "+ Angka1);
+        System.out.println("nilai type data float adalah "+ Angka2);
+        
+    }
+    
+}
