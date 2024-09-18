757d7dfdc98f4a1ef8a598ad92f8d6997c316a8a
Isdayunisari <isdai041@gmail.com>
2024-09-18 21:11:41 +0800

    Create day17.java

---
+++ day17.java
@@ -0,0 +1,15 @@
+package Days17;
+
+public class Days17 {
+    public static void main (String[] args) {
+        char hurufdepan = 'I';
+        char hurufbelakang = 'A';
+        System.out.println("hurufdepan type data char adalah"+ hurufdepan);
+        System.out.println("hurufbelakang type data char adalah"+ hurufbelakang);
+        
+    }
+        
+        
+        
+        
+}
