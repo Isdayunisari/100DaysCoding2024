698f93fa18f552afc405d601fe00bf43c37b42ad
Isdayunisari <isdai041@gmail.com>
2024-09-19 21:40:44 +0800

    Create day18.java

---
+++ day18.java
@@ -0,0 +1,12 @@
+package Days18;
+
+public class Days18 {
+    public static void main (String[] args) {
+        boolean nilai1 = false;
+        boolean nilai2 = true;
+        System.out.println("nilai1 "+ nilai1);
+        System.out.println("nilai2 "+ nilai2);
+    
+    }
+    
+}
