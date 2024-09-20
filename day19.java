a57fbe822018d7839fb7b60ce819f0922537afdb
Isdayunisari <isdai041@gmail.com>
2024-09-20 19:50:45 +0800

    Create day19.java

---
+++ day19.java
@@ -0,0 +1,10 @@
+package Days19;
+
+public class Days19 {
+    public static void main (String[] args) {
+        String hobby = "volly";
+        System.out.println("hobby "+ hobby);
+        
+    }
+    
+}
