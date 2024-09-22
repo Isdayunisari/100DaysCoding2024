fd9dd38e24920a84fe08cdf61f93fef2a056f503
Isdayunisari <isdai041@gmail.com>
2024-09-22 21:51:12 +0800

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,11 @@
+package Days21;
+
+public class Days21 {
+     public static void main (String[] args) {
+         Short Angka = 4 ;
+         System.out.println("Angka "+ Angka);
+         
+     }
+    
+    
+}
