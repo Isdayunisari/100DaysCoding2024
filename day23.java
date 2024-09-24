9f5cebc5b3b1fbeefd667d8190b9155d2f35f0fc
Isdayunisari <isdai041@gmail.com>
2024-09-24 21:00:22 +0800

    Create day23.java

---
+++ day23.java
@@ -0,0 +1,10 @@
+package Days23;
+
+public class Days23 {
+    public static void main (String[] args) {
+        Long a = 2l;
+        System.out.println("a "+ a);
+        
+    }
+        
+        
