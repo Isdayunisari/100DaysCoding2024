705678ff22eca222fb94e390726876e22ecd1817
Isdayunisari <isdai041@gmail.com>
2024-09-25 22:10:58 +0800

    Create day24.java

---
+++ day24.java
@@ -0,0 +1,11 @@
+package Days24;
+
+public class Days24 {
+    public static void main (String[] args) {
+        Float lala = 22.4f;
+        System.out.println("lala "+ lala);
+        
+    }
+        
+    
+}
