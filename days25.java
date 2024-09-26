1f4a1a70a3180b265097a683591f07e9f8c765dc
Isdayunisari <isdai041@gmail.com>
2024-09-26 18:59:01 +0800

    Create days25.java

---
+++ days25.java
@@ -0,0 +1,10 @@
+package Days25;
+
+public class Days25 {
+    public static void main (String[] args) {
+        Double loly = 14.4;
+        System.out.println("loly "+ loly);
+        
+    }
+    
+}
