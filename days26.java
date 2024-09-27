ca654f62563d4783e24984402cedb84520fc9bb8
Isdayunisari <isdai041@gmail.com>
2024-09-27 22:11:06 +0800

    Create days26.java

---
+++ days26.java
@@ -0,0 +1,10 @@
+package Days26;
+
+public class Days26 {
+    public static void main (String[] args) {
+        Character kaka = '4';
+        System.out.println("kaka "+ kaka);
+        
+    }
+    
+}
