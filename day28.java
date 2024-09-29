4853ddd4e36d33251a5e2888e7cf746a54f7775f
Isdayunisari <isdai041@gmail.com>
2024-09-29 20:30:39 +0800

    Create day28.java

---
+++ day28.java
@@ -0,0 +1,11 @@
+package Days28;
+
+public class Days28 {
+    public static void main (String[] args) {
+        Boolean angka = true;
+        System.out.println("angka "+ angka);
+        
+    }
+    
+    
+}
