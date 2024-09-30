378580724854c04f62ffc9c3558b737c1e7dd741
Isdayunisari <isdai041@gmail.com>
2024-09-30 23:54:50 +0800

    Create day29.java

---
+++ day29.java
@@ -0,0 +1,20 @@
+package Days29;
+import java.math.BigInteger;
+public class Days29 {
+    
+    public static void main(String[] args) {
+    
+   
+        
+        BigInteger ab = BigInteger.valueOf(12345678);
+        BigInteger ac = BigInteger.valueOf(234567);
+        BigInteger ad = BigInteger.valueOf(34567);
+        System.out.println(ab);
+        System.out.println(ac);
+        System.out.println(ad);
+        
+
+    
+   }
+
+}
