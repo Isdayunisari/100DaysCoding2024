368be46cdf5471d2121c45d231ba676fe4367f42
Isdayunisari <isdai041@gmail.com>
2024-10-01 21:55:27 +0800

    Create day30.java

---
+++ day30.java
@@ -0,0 +1,9 @@
+import java.math.BigDecimal;
+public class Days30 {
+    public static void main (String[] args) {
+        BigDecimal BanyakSekali = new BigDecimal (222.22222);
+        System.out.printf("BanyakSekali %f ", BanyakSekali);
+    }
+    
+    
+}
