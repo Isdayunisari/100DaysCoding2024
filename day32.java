3bbe6de4b1e03b7a840d7b70d87861e5ccc88750
Isdayunisari <isdai041@gmail.com>
2024-10-03 20:13:31 +0800

    Create day32.java

---
+++ day32.java
@@ -0,0 +1,11 @@
+package Days32;
+
+public class Days32 {
+    public static void main(String[] arga) {
+        byte ab = 14;
+        String sama = Byte.toString(ab);
+        System.out.println("sama : "+ ab);
+        
+    }
+    
+}
