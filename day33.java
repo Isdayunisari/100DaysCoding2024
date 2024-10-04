8c2725bfaf1a18be3dea7d896da0aa274a3ff5ec
Isdayunisari <isdai041@gmail.com>
2024-10-04 20:31:29 +0800

    Create day33.java

---
+++ day33.java
@@ -0,0 +1,15 @@
+package Days33;
+
+public class Days33 {
+    public static void main(String[] args){
+        int a = 44;
+        System.out.println("nilai int = "+ a);
+        long b = (long)a;
+        System.out.println("nilai casting int menjadi long = " + b);
+        //int ke double 
+        
+    }
+    
+    
+    
+}
