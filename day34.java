7d796f54ecf1d2630360c2bb0fa2bcca38e1c926
Isdayunisari <isdai041@gmail.com>
2024-10-05 23:35:03 +0800

    Create day34.java

---
+++ day34.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class days34 {
+	public static void main ( String[] args ){
+		
+		Scanner input =  new Scanner(System.in ); 
+		
+		System.out.println( " Masukkan Nama hewan : " );
+		String nama = input.nextLine();
+
+		System.out.println( " Masukkan Umur hewan : ");
+		int umur = input.nextInt();
+
+	}
+}
