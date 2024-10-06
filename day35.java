590a1e0f46d34a1f4269ed764b7f2fd1b32910c1
Isdayunisari <isdai041@gmail.com>
2024-10-06 22:37:01 +0800

    Create day35.java

---
+++ day35.java
@@ -0,0 +1,18 @@
+package Days35;
+
+public class Daya35 {
+    public static void main(String[] args) {
+        int a;
+        short b = 3;
+        short c = 4;
+        //penjumlahan
+        a = b + c;
+        System.out.println("hasil penjumlaha adalah "+ a);
+        //pengurangan
+        a = b - c;
+        System.out.println("hasil pengurangan adalah "+ a);
+        
+        
+    }
+    
+}
