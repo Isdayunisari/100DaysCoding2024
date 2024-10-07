547a06107b3545899cfc1b5771937d6482f24e3a
Isdayunisari <isdai041@gmail.com>
2024-10-07 20:22:49 +0800

    Create day36.java

---
+++ day36.java
@@ -0,0 +1,20 @@
+package Days36;
+
+public class Days36 {
+    public static void main(String[] args ) {
+        //perkalian
+        int perkalian = 4 * 2;
+        System.out.println("hasil perkalian "+ perkalian);
+        //output 8
+        
+        //pembagian
+        int pembagian = 4 / 2;
+        System.out.println("hasil pembagian "+ pembagian);
+        //output 2
+        
+        //modulo 
+        int modulo = 20 % 3;
+        System.out.println("hasil pembagian sisa atau modulo "+ modulo);
+        //sisa pembagian atau modulo adalah 2
+}
+}
