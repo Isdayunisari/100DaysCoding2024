dc8bfe21ecf894dcb5aab3c6fd7ad8527c0a734e
Isdayunisari <isdai041@gmail.com>
2024-10-08 22:39:34 +0800

    Create day37.java

---
+++ day37.java
@@ -0,0 +1,13 @@
+package Days37;
+
+public class Days37 {
+    public static void main(String[] args ){
+        double h = 2.5; //mengisi h dengan nilai 2.5
+        h +=1.2;
+        h -=2.1;
+        System.out.println("sisa nilai h: "+ h);
+        
+    }
+        
+}
+    
