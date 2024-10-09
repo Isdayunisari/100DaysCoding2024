ddbbdf662f4f2658de9a2a7e6519a6dbe4911169
Isdayunisari <isdai041@gmail.com>
2024-10-09 21:26:55 +0800

    Create day38.java

---
+++ day38.java
@@ -0,0 +1,15 @@
+package Days38;
+
+public class Days38 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 14;
+        //Operator Pengisian dan Perkalian
+        a *= b;
+        System.out.println(" Hasil Perkalian " + a);
+
+        //Operator Pengisian dan Pembagian
+        a /= b;
+        System.out.println(" Hasil Pembagian " + a);
+    }
+}
