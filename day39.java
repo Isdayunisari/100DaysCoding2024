15d724a5829cb77ad701acb060e04c59e429c1ab
Isdayunisari <isdai041@gmail.com>
2024-10-10 22:34:56 +0800

    Create day39.java

---
+++ day39.java
@@ -0,0 +1,14 @@
+package Days39;
+
+public class Days39 {
+    public static void main (String[] args) {
+        int a = 5;
+        int b = 2;
+        
+        int modulo = a%b;
+        
+        System.out.println("hasil modulo "+ modulo);
+        
+    }
+    
+}
