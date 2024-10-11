9490b3ce1d0166632c405d648ab82c4381213ff6
Isdayunisari <isdai041@gmail.com>
2024-10-11 23:21:50 +0800

    Create day40.java

---
+++ day40.java
@@ -0,0 +1,15 @@
+package Days40;
+
+public class Days40 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        System.out.println(a>b);
+        int c = 2;
+        int d = 3;
+        System.out.println(c<a);
+        
+    }
+        
+    
+}
