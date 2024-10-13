2e0d660097250e9a56eb30ccc493f5187637460a
Isdayunisari <isdai041@gmail.com>
2024-10-13 20:54:02 +0800

    Create day42.java

---
+++ day42.java
@@ -0,0 +1,14 @@
+public class Days42 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        boolean c = (a == b);
+        System.out.println(c);
+        
+        int d = 5;
+        int e = 6;
+        boolean g = (d != e);
+        System.out.println(g);
+        
+    }
+}
