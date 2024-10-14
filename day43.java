1f5e2fa64ceb97006d1882710086fdf08328477b
Isdayunisari <isdai041@gmail.com>
2024-10-14 23:02:52 +0800

    Create day43.java

---
+++ day43.java
@@ -0,0 +1,18 @@
+package Days43;
+
+public class Days43 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        boolean c = (a<=b);
+        System.out.println(c);
+        int e = 4;
+        int f = 2;
+        boolean g = (e>=f);
+        System.out.println(g);
+        
+        
+        
+    }
+}
+       
