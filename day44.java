9412f38a70f55b54fa37fd8404e432dad17d26e6
Isdayunisari <isdai041@gmail.com>
2024-10-15 21:31:25 +0800

    Create day44.java

---
+++ day44.java
@@ -0,0 +1,12 @@
+package Days44;
+
+public class Days44 {
+    public static void main(String[] args) {
+        boolean la,li;
+        la = true; 
+        li = false;
+        System.out.println(la && li);
+        
+    }
+    
+}
