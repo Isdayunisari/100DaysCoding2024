ceab45b5fd48855828e66b9556a9760d344d1b3d
Isdayunisari <isdai041@gmail.com>
2024-10-16 21:45:36 +0800

    Create day45.java

---
+++ day45.java
@@ -0,0 +1,15 @@
+package Days45;
+
+public class Days45 {
+    public static void main(String[] args) {
+        boolean a,b;
+        a = true;
+        b = false;
+        
+        System.out.println(a||b);
+        
+    }
+        
+        
+    
+} 
