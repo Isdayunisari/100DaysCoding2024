003cec1a2ae8f290b1f3190dd26ae2e0d9d17454
Isdayunisari <isdai041@gmail.com>
2024-10-17 22:02:25 +0800

    Create day46.java

---
+++ day46.java
@@ -0,0 +1,10 @@
+package Days46;
+
+public class Days46 {
+    public static void main(String[] args) {
+        boolean a = true;
+        System.out.println("nilai boolean menggunakan negasi "+ !a);
+        
+    }
+    
+}
