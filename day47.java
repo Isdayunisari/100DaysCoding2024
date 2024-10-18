dae14f23709f83bcb1bfc48742098f572ec25e06
Isdayunisari <isdai041@gmail.com>
2024-10-18 21:28:47 +0800

    Create day47.java

---
+++ day47.java
@@ -0,0 +1,13 @@
+package Days47;
+
+public class Days47 {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a > 5) {
+        System.out.println("angka positif");
+        
+    }
+        
+    }
+    }
+
