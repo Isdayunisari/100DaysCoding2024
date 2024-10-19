b5928975c9fe9f6fc704b58f7028bde2ef2957eb
Isdayunisari <isdai041@gmail.com>
2024-10-19 22:37:22 +0800

    Create day48.java

---
+++ day48.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Days48 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        int a = scanner.nextInt();
+        if (a > 5) {
+            System.out.println("angka positif");
+        } else if (a < 4) {
+            System.out.println("angka negatif");
+        
+    }
+        
+        
+    }
+}
