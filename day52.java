30c1df026edd063eb5fb09d946dccba684cf707f
Isdayunisari <isdai041@gmail.com>
2024-10-23 23:16:44 +0800

    Create day52.java

---
+++ day52.java
@@ -0,0 +1,13 @@
+package Days52;
+
+public class Days52 {
+    public static void main(String[] args) {
+        
+    int a = 10;
+    int b = 20;
+
+int max = (a > b) ? a : b; // max akan berisi 20
+        System.out.println(max);
+    
+}
+}
